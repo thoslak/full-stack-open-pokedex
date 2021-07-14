12d48a925cb4f2a34218a605a52573a4a330a90a
Thor Aslak Eggen <thoslak_95@hotmail.com>
2021-07-14 16:21:46 +0200

    added health check

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express')
+const pjson = require('./package.json')
+
 const app = express()
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send(pjson.version)
+})
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
